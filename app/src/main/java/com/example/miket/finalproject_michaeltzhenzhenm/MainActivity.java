d6cfb63cad3a7435d59d8bbdfa44091e66ebdb21
Michael Tarnow <miketar10@gmail.com>
2017-04-02 19:33:08 -0400

    Login and Spinner Added

--- app/src/main/java/com/example/miket/finalproject_michaeltzhenzhenm/MainActivity.java
+++ app/src/main/java/com/example/miket/finalproject_michaeltzhenzhenm/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.miket.finalproject_michaeltzhenzhenm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner=(Spinner) findViewById(R.id.spinner);
+        adapter= ArrayAdapter.createFromResource(this, R.array.levles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(),parent.getItemIdAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
     }
 }
