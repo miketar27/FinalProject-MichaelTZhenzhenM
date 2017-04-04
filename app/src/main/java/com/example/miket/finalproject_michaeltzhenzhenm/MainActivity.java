530fca9fff3ff0cdc82c1c558498348a9104dee1
Michael Tarnow <miketar10@gmail.com>
2017-04-04 14:05:07 -0400

    Adding Contact List

--- app/src/main/java/com/example/miket/finalproject_michaeltzhenzhenm/MainActivity.java
+++ app/src/main/java/com/example/miket/finalproject_michaeltzhenzhenm/MainActivity.java
@@ -2,18 +2,39 @@ package com.example.miket.finalproject_michaeltzhenzhenm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ListView lv = (ListView) findViewById(R.id.list_view_player);
+        ArrayList<String> arrayPlayers = new ArrayList<>();
+        arrayPlayers.addAll(Arrays.asList(getResources().getStringArray(R.array.players)));
+
+        adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayPlayers);
+        lv.setAdapter(adapter);
 
         Spinner levels = (Spinner) findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> levelsAdapter = ArrayAdapter.createFromResource(
@@ -33,7 +54,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+
+//        // Associate searchable configuration with the SearchView
+//        SearchManager searchManager =
+//                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView =
+//                (SearchView) menu.findItem(R.id.search).getActionView();
+//        searchView.setSearchableInfo(
+//                searchManager.getSearchableInfo(getComponentName()));
 
 
     }
+
+
 }
