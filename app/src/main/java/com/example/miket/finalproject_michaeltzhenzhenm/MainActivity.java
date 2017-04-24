468e8cae898c53b2b5e5ed42ed2466e9d0f8b623
Sean Ma <sean823ma@gmail.com>
2017-04-24 17:33:44 -0400

    Testing Push

--- app/src/main/java/com/example/miket/finalproject_michaeltzhenzhenm/MainActivity.java
+++ app/src/main/java/com/example/miket/finalproject_michaeltzhenzhenm/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //test
 
         database = FirebaseDatabase.getInstance();
         playerRef = database.getReference("players");
